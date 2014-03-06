737fa08500bc2933ceb61d62d77f5408cd0ab040
Bin Ren <bin.ren@gmail.com>
2014-03-06 07:23:00 +0000

    try this fix in CheckBlock

--- src/main.cpp
+++ src/main.cpp
@@ -2142,15 +2142,7 @@ bool CBlock::CheckBlock(CValidationState &state, bool fCheckPOW, bool fCheckMerk
     }
 
     // Check proof of work matches claimed amount
-    uint256 hash = GetHash();
-    int nHeight;
-    map<uint256, CBlockIndex*>::iterator mi = mapBlockIndex.find(hash);
-    if (mi != mapBlockIndex.end())
-         nHeight = (*mi).second->nHeight;
-    else
-        nHeight = 0;
-
-    if (fCheckPOW && nHeight > 44877 && !CheckProofOfWork(GetPoWHash(), nBits))
+    if (fCheckPOW && vtx[0].vin[0].scriptSig[0] > 44877 && !CheckProofOfWork(GetPoWHash(), nBits))
         return state.DoS(50, error("CheckBlock() : proof of work failed"));
 
     // Check timestamp
